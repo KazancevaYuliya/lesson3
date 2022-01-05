3d7840df4ee068e48a380f371a15293ec021f3b9
KazancevaYuliya <87359439+KazancevaYuliya@users.noreply.github.com>
2022-01-05 20:26:26 +0300

    Add files via upload

---
+++ ДЗ/script.js
@@ -0,0 +1,130 @@
+// --Задача 1--
+
+//let arr = [5, 4, 3, -3, -10, -1, 8, -20, 0]
+//let arr2 = [-7, 6, -5, -8, 9, -2, 4, 28, 1]
+
+//function findPositiveNumbers(arr = []) {
+//let arrNew = []
+
+//for (let item of arr) {
+//if (item > 0) {
+//arrNew.push(item)
+//}
+//}
+
+//return arrNew
+//}
+//console.log(findPositiveNumbers(arr))
+//console.log(findPositiveNumbers(arr2))
+
+
+// --Задача 2--
+
+//let arr = [5, 4, 3, 8, 0]
+
+//function filterFor(arr, a) {
+//let arrNew = []
+
+//for (let item of arr) {
+//if (item >= a) {
+//arrNew.push(item)
+//}
+//}
+
+//return arrNew
+//}
+
+//console.log(filterFor(arr, 5))
+//console.log(filterFor(arr, 10))
+//console.log(filterFor(arr, 3.11))
+
+
+//  --1.--
+
+
+//const numbers = [25, 30, 55, 110, 40, 10]
+
+//function calcSumNumbers() {
+//let sum = 0
+//for (let item of numbers) {
+//if (item > 0) {
+//sum = sum + item
+//}
+//}
+
+//return sum
+//}
+//console.log(calcSumNumbers([25, 30, 55, 110, 40, 10]))
+
+// --1.1--
+
+//const numbers = [27, 30, 55, 110, 40, 10]
+
+//function calcSumNumbersEven() {
+//let sum = 0;
+//for (let i = 0; i < numbers.length; i++) {
+//if (numbers[i] % 2 == 0) {
+//sum += numbers[i];
+//}
+//}
+//return sum
+//}
+//console.log(calcSumNumbersEven([27, 30, 55, 110, 40, 10]))
+
+
+// --2.--
+
+//const numbers = [130, 40, 37, 248, 45]
+
+//function calcSumNumbersAverage() {
+//let sum = 0
+//for (let i = 0; i < numbers.length; i++) {
+//sum += numbers[i]
+//result = sum / numbers.length
+//}
+
+//return result
+//}
+//console.log(calcSumNumbersAverage([130, 40, 37, 248, 45]))
+
+
+// --3.--
+
+//let arr1 = [5, 4, 3, 8, 0]
+
+//function resultArr() {
+//let limit = 5
+//let arr2 = []
+//for (let item of arr1) {
+//if (item >= limit) {
+//arr2.push(item)
+//}
+//}
+//return arr2
+//}
+//console.log(resultArr([5, 4, 3, 8, 0]))
+
+// --4.--
+
+//const users = [
+    //{ name: 'Yulya', age: 36 },
+    //{ name: 'Kolya', age: 38 },
+    //{ name: 'Tanya', age: 53 },
+    //{ name: 'Masha', age: 9 },
+//]
+
+//function showUserName(name, age) {
+    //for (let user of users) {
+        //if (user.age > 10) {
+            //result = user.name
+            //console.log(result)
+        //}
+    //}
+
+    //return result
+//}
+//console.log(showUserName([]))
+
+
+
+
